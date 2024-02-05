823e3a5b17ff2c7f939110e75336c85c64832b8d
Sevim Ezgi Bulamacı <ezgi.bulamaci@kentkart.com>
2024-02-05 19:41:40 +0300

    Added payment information to success screen

--- src/components/SuccessComponent.js
+++ src/components/SuccessComponent.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import './SuccessFailPage.css';
 
 function SuccessComponent() {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const amount = queryParams.get('amount');
+  const userId = queryParams.get('userId');
+  const network = queryParams.get('network');
+  const currency = queryParams.get('currency');
+
   return (
     <div className="status-page success">
       <div className="status-icon">&#10003;</div>
       <h2>Payment Successful</h2>
       <p>Your payment has been processed successfully.</p>
+      <div className="details-container">
+        <p className="detail"><div className='strong'>Network:</div> {network}</p>
+        <p className="detail"><div className='strong'>Currency:</div> {currency}</p>
+        <p className="detail"><div className='strong'>Amount:</div> {amount}</p>
+        <p className="detail"><div className='strong'>User ID:</div> {userId}</p>
+      </div>
     </div>
   );
 }
